b14f152a564883372b29adcffe337d5093e1d9ce
yooninkwon <yooninkwon@naver.com>
2024-12-13 17:30:28 +0900

    게시판 프로젝트 시작

---
+++ src/main/java/com/tech/board/MyprjBoardApplication.java
@@ -0,0 +1,13 @@
+package com.tech.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprjBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprjBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/board/MyprjBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprjBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
